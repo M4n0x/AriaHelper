317305904e5f4287b57a4db10d287d1e70548a36
M4n0x <M4n0x@users.noreply.github.com>
2020-10-17 17:35:04 +0200

    Add serilazable classes

--- app/src/main/java/ch/hearc/ariahelper/models/Attribute.kt
+++ app/src/main/java/ch/hearc/ariahelper/models/Attribute.kt
@@ -1,7 +1,10 @@
 package ch.hearc.ariahelper.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * An attribute is a simple name->value class to stock attributes such as strenght, charisma, etc...
  */
-data class Attribute(val name : String, val value : Int=0) {
-}
+@Parcelize
+data class Attribute(val name : String, val value : Int=0) : Parcelable
--- app/src/main/java/ch/hearc/ariahelper/models/Character.kt
+++ app/src/main/java/ch/hearc/ariahelper/models/Character.kt
@@ -1,17 +1,21 @@
 package ch.hearc.ariahelper.models
 
 import android.graphics.drawable.Drawable
+import android.os.Parcelable
 import ch.hearc.ariahelper.models.commonpool.ItemBasicPool
 import ch.hearc.ariahelper.models.commonpool.SkillBasicPool
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Stock the character with its context (item and skills)
  */
-data class Character(val name: String, val level: Int,
-                     val picture: Drawable ? = null,
+@Parcelize
+data class Character(val name: String,
+                     val level: Int,
+                     val picture: String? = null,
                      val money : MoneyValue,
                      val itemList : MutableList<Item>,
-                     val skillList : MutableList<Skill>) {
+                     val skillList : MutableList<Skill>) : Parcelable {
 
     //basic constructor with just a name
     constructor(name: String) : this(name, 0, null, MoneyValue(0,0,0), ItemBasicPool.ITEMS, SkillBasicPool.SKILLS)
